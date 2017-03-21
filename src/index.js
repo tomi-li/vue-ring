02605b671a4238f809476ccdac9b734fd8d09cd6
tomi <tomi@ccfocusone.com>
2017-03-21 17:25:05 +0800

    [feature] remove useless files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * All Codes below are Lifetime Warranted by Tomi since 21/03/2017.
+ */
+
+import circle from './circle.vue';
+
+export { circle };
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
@@ -15,9 +15,6 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
@@ -51,10 +48,10 @@ module.exports = {
     hints: false
   },
   devtool: '#eval-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
